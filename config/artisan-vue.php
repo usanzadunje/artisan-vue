d8c16f429bba9f94aaf3e0d471bea2ac203e41ea
Dusan Djordjevic <dussann1997@live.com>
2022-02-02 17:30:40 +0100

    Updated config description

--- config/artisan-vue.php
+++ config/artisan-vue.php
@@ -2,25 +2,69 @@
 /*
  * Configuration file for Artisan Vue package
  *
- * Here you can specify name of the directories where certain Vue assets live.
+ * Here you can specify name/path of the directories where certain Vue resources live.
  * This names will be used to store content created by the package.
  */
+
 return [
-    // Root directory where your Vue app lives
+    /*
+    |--------------------------------------------------------------------------
+    | Path to Vue root
+    |--------------------------------------------------------------------------
+    |
+    | Path to your Vue application root folder, where resources will live.
+    |
+    */
     'vue_root_dir' => resource_path("js"),
 
-    // Name of the folder which stores views
+    /*
+    |--------------------------------------------------------------------------
+    | Name of the views directory
+    |--------------------------------------------------------------------------
+    |
+    | Name of the directory which stores your views.
+    |
+    */
     'views_dir' => 'views',
 
-    // Name of the folder which stores components
+    /*
+    |--------------------------------------------------------------------------
+    | Name of the components directory
+    |--------------------------------------------------------------------------
+    |
+    | Name of the directory which stores your components.
+    |
+    */
     'components_dir' => 'components',
 
-    // Name of the folder which stores composables(hooks)
+    /*
+    |--------------------------------------------------------------------------
+    | Name of the composables(hooks) directory
+    |--------------------------------------------------------------------------
+    |
+    | Name of the directory which stores your composables(hooks).
+    |
+    */
     'composables_dir' => 'composables',
 
-    // Path to the folder which stores Vuex modules
+    /*
+    |--------------------------------------------------------------------------
+    | Path to the modules directory
+    |--------------------------------------------------------------------------
+    |
+    | Path to directory which stores your Vuex modules,
+    | relative to `vue_root_dir` specified above.
+    |
+    */
     'vuex-modules_dir' => 'store/modules',
 
-    // Name of the folder which stores services
+    /*
+    |--------------------------------------------------------------------------
+    | Name of the services directory
+    |--------------------------------------------------------------------------
+    |
+    | Name of the directory which stores your services.
+    |
+    */
     'services_dir' => 'services',
 ];
